058664c78ed983c889326d366dfe1081041ad559
RabidMexican <39406726+RabidMexican@users.noreply.github.com>
2021-07-26 15:32:57 +0200

    Automatic detection of all data files

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import os
+
 import pyreadstat as py
 
-DATA_FILES = ['demographic', 'diet', 'physical']
 DATA_DIRECTORY = "data"
 RESULTS_DIRECTORY = "results"
 
-print('STARTING...')
+print('Starting treatment of data files : \n')
 
-for data_file in DATA_FILES:
+for data_file in os.listdir(DATA_DIRECTORY):
 
-    print(f"Creating results from {data_file}.sav ...")
+    file_name = os.path.splitext(data_file)[0]
+    print(f"=== {file_name} ===")
 
-    df, meta = py.read_sav(f"{DATA_DIRECTORY}/{data_file}.sav")
-    df.to_csv(f'{RESULTS_DIRECTORY}/{data_file}.csv')
+    df, meta = py.read_sav(f"{DATA_DIRECTORY}/{data_file}")
+    df.to_csv(f'{RESULTS_DIRECTORY}/{file_name}.csv')
 
-    print(f"{data_file}.csv created successfully !")
+    print(f"{data_file}.csv created successfully !\n")
