c451bdef9e4dbc0fa0551ea4a1fd2196aed28faa
RabidMexican <39406726+RabidMexican@users.noreply.github.com>
2021-07-26 15:25:11 +0200

    Updated logs

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ RESULTS_DIRECTORY = "results"
 print('STARTING...')
 
 for data_file in DATA_FILES:
-    print('Starting next file...')
+
+    print(f"Creating results from {data_file}.sav ...")
 
     df, meta = py.read_sav(f"{DATA_DIRECTORY}/{data_file}.sav")
     df.to_csv(f'{RESULTS_DIRECTORY}/{data_file}.csv')
+
+    print(f"{data_file}.csv created successfully !")
